1bc3cdb9798bac7f1fb1e3c162429867980a2914
牧野颯太 <sm96kyoto@gmail.com>
2018-08-04 22:49:14 +0900

    declare model's relationships

--- app/models/nega.rb
+++ app/models/nega.rb
@@ -1,2 +1,23 @@
 class Nega < ApplicationRecord
+  validates :type, presence: true
+  validates :description, presence: true
+
+  has_many :films, dependent: :destroy
+  accepts_nested_attributes_for :films, allow_destroy: true
+
+  def as_json(_opts = {})
+    {
+      id: id,
+      type: type,
+      description: description,
+      errors: errors,
+      film_photos: filmss.map do |x|
+        {
+          url: x.photo.url.absolute_url,
+          name: x.photo_file_name,
+          id: x.id
+        }
+      end
+    }
+  end
 end
