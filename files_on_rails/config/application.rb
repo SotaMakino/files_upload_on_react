b993c92934b0e01e9bf41ca345931672ef6da680
牧野颯太 <sm96kyoto@gmail.com>
2018-08-07 19:12:51 +0900

    re fix methods

--- files_on_rails/config/application.rb
+++ files_on_rails/config/application.rb
@@ -28,7 +28,7 @@ module FilesUploadDemo
         resource \
            '*',
            headers: :any,
-           methods: [get post options delete patch puts]
+           methods: %I[get post options delete patch puts]
       end
     end
 
