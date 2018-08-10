8df0dc3f3973d15392d899e59ea01f4b8ace6c25
牧野颯太 <sm96kyoto@gmail.com>
2018-08-10 18:14:50 +0900

    fix regexp

--- files_on_rails/app/models/film.rb
+++ files_on_rails/app/models/film.rb
@@ -6,8 +6,8 @@ class Film < ApplicationRecord
     convert_options: {
       all: '-interlace Plane'
     },
-    default_url: '/images/default_:style_photo.png'
+    default_url: './public/noimage_article.png'
 
   validates_attachment_presence :photo
-  validates_attachment_file_name :photo, matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]
+  validates_attachment_file_name :photo, matches: [/png\Z/, /jpe?g|JPE?G\Z/, /gif\Z/]
 end
