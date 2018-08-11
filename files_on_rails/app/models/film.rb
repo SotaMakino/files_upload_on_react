10473f3981654355f36d107afc9c9e13c9121fa8
牧野颯太 <sm96kyoto@gmail.com>
2018-08-11 11:42:36 +0900

    add user_id columns

--- files_on_rails/app/models/film.rb
+++ files_on_rails/app/models/film.rb
@@ -1,4 +1,5 @@
 class Film < ApplicationRecord
+  belongs_to :user
   belongs_to :nega, inverse_of: :films
   has_attached_file \
     :photo,
@@ -9,5 +10,5 @@ class Film < ApplicationRecord
     default_url: './public/noimage_article.png'
 
   validates_attachment_presence :photo
-  validates_attachment_file_name :photo, matches: [/png\Z/, /jpe?g|JPE?G\Z/, /gif\Z/]
+  validates_attachment_file_name :photo, matches: [/png|PNG\Z/, /jpe?g|JPE?G\Z/, /gif|GIF\Z/]
 end
