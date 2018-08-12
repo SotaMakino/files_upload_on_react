9791cafcdc8510a2bd088e672e3ef434e758767d
牧野颯太 <sm96kyoto@gmail.com>
2018-08-12 17:27:49 +0900

    adjust db

--- files_on_rails/db/schema.rb
+++ files_on_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_142656) do
+ActiveRecord::Schema.define(version: 2018_08_12_082654) do
 
   create_table "films", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "nega_id"
@@ -20,11 +20,20 @@ ActiveRecord::Schema.define(version: 2018_08_11_142656) do
     t.string "photo_content_type"
     t.integer "photo_file_size"
     t.datetime "photo_updated_at"
+    t.integer "user_id"
   end
 
   create_table "negas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "login"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
