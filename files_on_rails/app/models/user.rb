e20330f17259882c4fa2919ae5525fa04109e77d
牧野颯太 <sm96kyoto@gmail.com>
2018-08-12 18:06:25 +0900

    user auth backend done

--- files_on_rails/app/models/user.rb
+++ files_on_rails/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+	has_many :negas, dependent: :destroy
+	has_many :films, dependent: :destroy
+
+	validates :name, presence: true
+    validates :login, presence: true, uniqueness: true
 end
