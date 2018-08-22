96a6113b1c3aa4c80ef3d10cf57bd2d28f08e78f
牧野颯太 <sm96kyoto@gmail.com>
2018-08-22 22:59:06 +0900

    fix dir-tree

--- files_on_rails/db/migrate/20180804124247_create_films.rb
+++
@@ -1,9 +0,0 @@
-class CreateFilms < ActiveRecord::Migration[5.2]
-  def change
-    create_table :films do |t|
-      t.integer :nega_id
-
-      t.timestamps
-    end
-  end
-end
--- files_on_rails/db/migrate/20180805053515_rename_type_column_to_negas.rb
+++
@@ -1,5 +0,0 @@
-class RenameTypeColumnToNegas < ActiveRecord::Migration[5.2]
-  def change
-  	rename_column :negas, :type, :title
-  end
-end
