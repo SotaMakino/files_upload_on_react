c3031eb234b1e351b7ab9b0d8255419b56508caa
牧野颯太 <sm96kyoto@gmail.com>
2018-08-23 20:38:27 +0900

    change baseurl

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,8 +1,7 @@
-
 let axios = require('axios');
 
 let axiosClient = axios.create({
-  baseURL: 'https://negabook-server.herokuapp.com/'
+  baseURL: 'https://negabook-server.herokuapp.com/negabook-client/'
 });
 
 export default axiosClient;
