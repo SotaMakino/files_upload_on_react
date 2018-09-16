21091c29226c3dc07c79a1cec0d376617484bfd3
牧野颯太 <sm96kyoto@gmail.com>
2018-09-16 20:59:01 +0900

    fix react tool box for ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as theme from './toolbox/theme';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
-import theme from './toolbox/theme';
 import App from './App';
 import './index.css';
 
