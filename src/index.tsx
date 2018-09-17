23696d5810ca3dc9d8f84ed1d18805a7f6a473d2
牧野颯太 <sm96kyoto@gmail.com>
2018-09-17 14:52:04 +0900

    migrate App.js to TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Router } from 'react-router';
 import * as theme from './toolbox/theme';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import App from './App';
 import './index.css';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const history = createBrowserHistory()
 
 ReactDOM.render(
 	<ThemeProvider theme={theme}>
-	  <App />
+	  <Router history={history}>
+	    <App />
+		</Router>
 	</ThemeProvider>,
 	document.getElementById('root') as HTMLElement
 );
