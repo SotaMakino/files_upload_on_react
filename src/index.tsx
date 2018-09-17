829ba207a8d446d14233df8ccde45ad702b027ec
牧野颯太 <sm96kyoto@gmail.com>
2018-09-17 16:05:55 +0900

    fix index

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import * as ReactDOM from 'react-dom';
 import { Router } from 'react-router';
 import * as theme from './toolbox/theme';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './App';
 import './index.css';
-import createBrowserHistory from 'history/createBrowserHistory';
 
 const history = createBrowserHistory()
 
 ReactDOM.render(
-	<ThemeProvider theme={theme}>
-	  <Router history={history}>
-	    <App />
-		</Router>
-	</ThemeProvider>,
+		<ThemeProvider theme={theme}>
+			<Router history={history}>
+				<App />
+			</Router>
+		</ThemeProvider>,
 	document.getElementById('root') as HTMLElement
 );
